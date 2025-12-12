1be7dcd5e061b65e18de79dace9ea2fea6a54726
sathyanarendrareddybudati <sathyanarendrareddybudati@gmail.com>
2025-12-12 15:05:16 +0000

    bug fix in controllers

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -21,11 +21,11 @@ class DashboardController extends Controller
             case 'admin':
                 $this->redirect('/admin');
                 break;
-            case 'owner':
-                $this->render('dashboard/owner', ['pageTitle' => 'Owner Dashboard']);
+            case 'pet_owner':
+                $this->render('dashboard/owner', ['pageTitle' => 'Customer Dashboard']);
                 break;
-            case 'caretaker':
-                $this->render('dashboard/caretaker', ['pageTitle' => 'Caretaker Dashboard']);
+            case 'service_provider':
+                $this->render('dashboard/caretaker', ['pageTitle' => 'Service Provider Dashboard']);
                 break;
             default:
                 Session::flash('error', 'Invalid user role.');
